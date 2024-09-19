eb394034a046f3337399e95c633be5e5b262609e
Farothtc <mvkirimi@gmail.com>
2024-09-19 17:55:22 +0300

    Added eventListener to the button

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,11 +1,21 @@
+import memesData from "../memesData";
+
 export function Meme() {
+  function handleImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
   return (
     <div className="meme">
-      <form action="#" className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form-input" />
         <input type="text" placeholder="Bottom text" className="form-input" />
-        <button className="form-button">Get a new meme image</button>
-      </form>
+        <button className="form-button" onClick={handleImage}>
+          Get a new meme image
+        </button>
+      </div>
     </div>
   );
 }
