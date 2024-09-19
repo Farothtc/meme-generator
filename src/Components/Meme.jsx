d9ee32f6b4934e0d1e84160dbd01d202b74de7f7
Farothtc <mvkirimi@gmail.com>
2024-09-19 16:52:20 +0300

    Initial commit

---
+++ src/Components/Meme.jsx
@@ -0,0 +1,11 @@
+export function Meme() {
+  return (
+    <div className="meme">
+      <form action="#" className="form">
+        <input type="text" placeholder="Top text" className="form-input" />
+        <input type="text" placeholder="Bottom text" className="form-input" />
+        <button className="form-button">Get a new meme image</button>
+      </form>
+    </div>
+  );
+}
