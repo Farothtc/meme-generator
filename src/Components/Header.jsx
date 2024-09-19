d6c604ea2e96a61b933c0cbaa0c3607bda81e691
Farothtc <mvkirimi@gmail.com>
2024-09-19 14:48:38 +0300

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <div className="header">
+      <span className="header-left">
+        <img src="./face.png" />
+        <h1>Meme Generator</h1>
+      </span>
+      <h2>React Course - Project 3</h2>
+    </div>
+  );
+}
