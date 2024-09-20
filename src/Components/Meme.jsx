172fb3c65d4ba1d620528dc9d7d03ebd34bb8790
Farothtc <mvkirimi@gmail.com>
2024-09-20 14:43:57 +0300

    useState inserted into Meme component

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,21 +1,27 @@
 import memesData from "../memesData";
+import { useState } from "react";
 
 export function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+
   function handleImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
-    console.log(url);
+    setMemeImage(() => memesArray[randomNumber].url);
   }
+
   return (
-    <div className="meme">
-      <div className="form">
-        <input type="text" placeholder="Top text" className="form-input" />
-        <input type="text" placeholder="Bottom text" className="form-input" />
-        <button className="form-button" onClick={handleImage}>
-          Get a new meme image
-        </button>
+    <>
+      <div className="meme">
+        <div className="form">
+          <input type="text" placeholder="Top text" className="form-input" />
+          <input type="text" placeholder="Bottom text" className="form-input" />
+          <button className="form-button" onClick={() => handleImage()}>
+            Get a new meme image
+          </button>
+        </div>
+        <img src={memeImage} />
       </div>
-    </div>
+    </>
   );
 }
