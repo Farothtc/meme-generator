4691f25b38a3eeca1b8643921b80aad7f960c20f
Farothtc <mvkirimi@gmail.com>
2024-10-04 17:07:16 +0300

    topText and bottomText is rendered to UI

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -22,17 +22,45 @@ export function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevText) => ({
+      ...prevText,
+      [name]: value,
+    }));
+  }
+
+  console.log(meme);
+
   return (
     <>
       <div className="meme">
         <div className="form">
-          <input type="text" placeholder="Top text" className="form-input" />
-          <input type="text" placeholder="Bottom text" className="form-input" />
+          <input
+            type="text"
+            placeholder="Top text"
+            className="form-input"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Bottom text"
+            className="form-input"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
           <button className="form-button" onClick={() => handleImage()}>
             Get a new meme image
           </button>
         </div>
-        <img src={meme.randomImage} className="meme-image" />
+        <div className="meme">
+          <img src={meme.randomImage} className="meme--image" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
       </div>
     </>
   );
